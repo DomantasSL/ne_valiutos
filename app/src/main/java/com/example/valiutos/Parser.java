9e1f622834e9b0d959d64805d2e03e5da1ffb8c6
vulkan <domantas.stalgys@gmail.com>
2022-12-15 15:33:15 +0200

    papildytas

--- app/src/main/java/com/example/valiutos/Parser.java
+++ app/src/main/java/com/example/valiutos/Parser.java
@@ -21,18 +21,25 @@ public class Parser {
         }
 
         String result = "";
+        String result1 = "";
         try {
             JSONObject jData = new JSONObject(data);
 
-            JSONArray weather = jData.getJSONArray("forecastTimestamps");
-            JSONObject weather1 = weather.getJSONObject(0);
 
+                JSONArray weather = jData.getJSONArray("forecastTimestamps");
             JSONObject placeNode = jData.getJSONObject("place");
-
-            String time = weather1.getString("forecastTimeUtc");
-            String temp = weather1.getString("airTemperature");
             String administrativeDivision = placeNode.getString("administrativeDivision");
-            result = String.format("Location name: %s \n\nTime: %s \nTemperature: %s", administrativeDivision, time, temp);
+                result = String.format("location:%s \n",administrativeDivision);
+                for (int i = 0; i < weather.length(); i++) {
+                    JSONObject weather1 = weather.getJSONObject(i);
+
+                    String time = weather1.getString("forecastTimeUtc");
+                    String temp = weather1.getString("airTemperature");
+
+                    result1 = String.format("\nTime: %s \nTemperature: %s \n", time, temp);
+                    result = result + result1;
+                }
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
